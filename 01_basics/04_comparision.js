44a2d9ab54b4a55c8b9719b836a566f5802e0cd5
Devraj <devrajsinghddd2737@gmail.com>
2025-07-27 09:51:57 +0530

    changes

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -12,11 +12,12 @@ console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
 
+console.log("-------------");
+
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
 // === 
 
-// console.log("2" === 2);
 console.log("2" === 2);
