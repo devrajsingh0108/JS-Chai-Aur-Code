a19afc6f2f83b8ff5156a730988a1228306feaff
Devraj <devrajsinghddd2737@gmail.com>
2025-07-28 20:48:24 +0530

    switch statements

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,33 @@
+//Synatax for switch statement
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+//-----------------Example of switch case----------\\
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
